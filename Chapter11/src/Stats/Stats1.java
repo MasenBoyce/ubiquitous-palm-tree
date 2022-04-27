a33efe29ccca6f3403ead19155161d92d14bddb8
904169003 <904169003@L815WD220020.edu.cbe.ab.ca>
2022-04-27 12:01:08 -0600

    push

--- Chapter11/src/Stats/Stats1.java
+++ Chapter11/src/Stats/Stats1.java
@@ -17,7 +17,11 @@ public class Stats1
 		BufferedReader File;
 		
 		String file, readLine;
-		double Score, lScore, hScore, aScore;
+		double Score, aScore;
+		double lScore = 100;
+		double hScore = 0;
+		double tScore = 0;
+		double nScore = 0;
 		
 
 		System.out.println("Enter the name of the file: "); //Obtain file name from user
@@ -39,19 +43,45 @@ public class Stats1
 			
 			
 			
-			
 			while((readLine = File.readLine()) != null) 
 			{
 				stuName.add(readLine);
-				System.out.println(stuName + "\t");
 				
 				
 				readLine = File.readLine();
 				stuScore.add(readLine);
-				System.out.println(stuScore + "\t");
+
 				
 				Score = Double.parseDouble(readLine);
+				tScore += Double.parseDouble(readLine);
+				nScore += 1;
+				
+				
+				if (Score > hScore) 
+				{
+					hScore = Score;
+				}
+				
+				if (Score < lScore) 
+				{
+					lScore = Score;
+				}
+				
+				
+						
+				
 			}
+			
+			
+			aScore = tScore / nScore;
+			
+			
+			System.out.println("\n" + stuName + "\t");
+			System.out.println(stuScore + "\t");
+			System.out.println("\n\nHighest Score: " + hScore);
+			System.out.println("Lowest Score: " + lScore);
+			System.out.println("Average Score: " + aScore);
+			
 			
 			
 			
